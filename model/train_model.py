bf6aced576ebbb985f1442dae415a36cd9aa7de2
AbhinavGadde <abhinavgadde780@gmail.com>
2025-04-22 10:45:13 +0530

    Update train_model.py

---
+++ model/train_model.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.ensemble import IsolationForest
+import joblib
+from model.features import extract_features
+
+df = pd.read_csv("data/logs.csv")
+X = extract_features(df)
+
+model = IsolationForest(contamination=0.2, random_state=42)
+model.fit(X)
+
+joblib.dump(model, "model/insider_threat_model.pkl")
+print("✅ Model trained and saved.")
