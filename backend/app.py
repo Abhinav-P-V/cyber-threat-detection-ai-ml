03d79e9efb2c39161c69a7c1f03a56dff4173ccd
AbhinavGadde <abhinavgadde780@gmail.com>
2025-04-22 10:46:28 +0530

    Update app.py

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+from model.features import extract_features
+
+# Load trained model
+model = joblib.load("model/insider_threat_model.pkl")
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        input_df = pd.DataFrame([data])
+        features = extract_features(input_df)
+        prediction = model.predict(features)[0]
+        result = "Anomaly" if prediction == -1 else "Normal"
+        return jsonify({'threat': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
